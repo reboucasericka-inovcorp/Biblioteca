691cfd9ddffbf2ba2d13f6961ea5177a5aa5496a
reboucasericka-inovcorp <erica.reboucas@trainee.inovcorp.com>
2026-02-11 11:09:15 +0000

    Disable Vite in testing environment

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @if (! app()->environment('testing') && (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot'))))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
         @endif
 
