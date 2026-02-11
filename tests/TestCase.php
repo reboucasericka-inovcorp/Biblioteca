bc00aa5d813ce315929e6deb3cfebf98445a68cd
reboucasericka-inovcorp <erica.reboucas@trainee.inovcorp.com>
2026-02-11 10:46:00 +0000

    correction of tests/TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,12 +3,11 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use CreatesApplication;
-
 
 abstract class TestCase extends BaseTestCase
 {
-   
+    use CreatesApplication;
+
     protected function setUp(): void
     {
         parent::setUp();
